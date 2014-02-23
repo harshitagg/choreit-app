5f9c3cceddba0d046da0f84b5f1707a93a97a55b
Harshit Agarwal <harshitagarwal90@gmail.com>
2014-02-23 02:24:25 -0500

    Harshit | Refactored and fixed ChoreActivity.

--- choreit-app/src/main/java/com/pineapps/choreit/activity/ChoreActivity.java
+++ choreit-app/src/main/java/com/pineapps/choreit/activity/ChoreActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.*;
 import com.pineapps.choreit.ChoreItContext;
 import com.pineapps.choreit.R;
-import com.pineapps.choreit.domain.Chore;
 import com.pineapps.choreit.domain.PredefinedChore;
 import com.pineapps.choreit.service.ChoreService;
 import com.pineapps.choreit.service.PredefinedChoreService;
@@ -17,50 +16,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChoreActivity extends Activity {
-    private Button addChoreButton;
     private Spinner choresSpinner;
-    private View choreDescription;
     private ChoreService choreService;
-    private PredefinedChoreService predefinedChoreService;
-    private ArrayAdapter stringArrayAdapter;
-    private List<PredefinedChore> allPredefinedChores;
-    private List<String> predefinedChoreList;
-    private EditText description;
-    private Chore c1;
-    private Chore c2;
-    private Chore c3;
-    private List<Chore> chores;
-    private View addButton;
-    private ChoreItContext choreInstance;
+    private List<PredefinedChore> predefinedChoreList;
+    private List<String> predefinedChoreTitleList;
+    private EditText descriptionEditText;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chore_activity);
+
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(R.string.action_add_chore);
 
-        choreInstance = ChoreItContext.getInstance();
-        choreService = choreInstance.choreService();
-        predefinedChoreService = choreInstance.predefinedChoreService();
-        choresSpinner = (Spinner) findViewById(R.id.chore_name);
-        addChoreButton = (Button) findViewById(R.id.add_chore);
-        description = (EditText) findViewById(R.id.chore_description);
-        allPredefinedChores = predefinedChoreService.getAllPredifinedChores();
-        predefinedChoreList = new ArrayList<String>();
-        predefinedChoreList.add("Select Chore");
-        for (PredefinedChore predefinedChore : allPredefinedChores) {
-            predefinedChoreList.add(predefinedChore.title());
+        ChoreItContext choreItContext = ChoreItContext.getInstance();
+
+        descriptionEditText = (EditText) findViewById(R.id.chore_description);
+
+        choreService = choreItContext.choreService();
+
+        initPredefinedChores(choreItContext);
+        initChoreSpinner();
+        initAddChoreButton();
+    }
+
+    private void initPredefinedChores(ChoreItContext choreItContext) {
+        PredefinedChoreService predefinedChoreService = choreItContext.predefinedChoreService();
+        predefinedChoreList = predefinedChoreService.getAllPredifinedChores();
+        predefinedChoreTitleList = new ArrayList<String>();
+        predefinedChoreTitleList.add("Select Chore");
+        for (PredefinedChore predefinedChore : predefinedChoreList) {
+            predefinedChoreTitleList.add(predefinedChore.title());
         }
-        stringArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, predefinedChoreList);
-        choresSpinner.setAdapter(stringArrayAdapter);
+    }
+
+    private void initAddChoreButton() {
+        Button addChoreButton = (Button) findViewById(R.id.add_chore);
+        addChoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choresSpinner.getSelectedItem().toString().equals("Select Chore")) {
+                    Toast.makeText(getApplicationContext(), "Please Enter the Chore Details", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                choreService.addChore(String.valueOf(choresSpinner.getSelectedItem()),
+                        String.valueOf(descriptionEditText.getText()));
+                Toast.makeText(getApplicationContext(), "Chore Created", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initChoreSpinner() {
+        choresSpinner = (Spinner) findViewById(R.id.chore_name);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, predefinedChoreTitleList);
+        choresSpinner.setAdapter(adapter);
         choresSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
                 if (position == 0) {
                     return;
                 }
-                description.setText(allPredefinedChores.get(position - 1).description());
-
+                descriptionEditText.setText(predefinedChoreList.get(position - 1).description());
             }
 
             @Override
@@ -68,24 +85,10 @@ public class ChoreActivity extends Activity {
 
             }
         });
-        addChoreButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                if(choresSpinner.getSelectedItem().toString().equals("Select Chore")) {
-                    Toast.makeText(getApplicationContext(),"Please Enter the Chore Details",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                choreService.addChore(String.valueOf(choresSpinner.getSelectedItem()), String.valueOf(description.getText()));
-                Toast.makeText(getApplicationContext(),"Chore Created",Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
