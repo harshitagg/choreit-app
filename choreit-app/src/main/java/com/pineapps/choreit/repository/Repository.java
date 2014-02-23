c8b256a75712326d2e4a36c3f6be501ed82f0e5e
Harshit Agarwal <harshitagarwal90@gmail.com>
2014-02-23 03:17:21 -0500

    Harshit | Creating default chores.

--- choreit-app/src/main/java/com/pineapps/choreit/repository/Repository.java
+++ choreit-app/src/main/java/com/pineapps/choreit/repository/Repository.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.pineapps.choreit.repository.PredefinedChoreRepository.PREDEFINED_CHORE_TABLE_NAME;
+
 public class Repository extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "ChoreIt.db";
@@ -22,10 +24,19 @@ public class Repository extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         for (ChoreItRepository choreItRepository : choreItRepositories) {
             choreItRepository.onCreate(sqLiteDatabase);
+            if (choreItRepository.getClass() == PredefinedChoreRepository.class) {
+                createPredefinedChores(sqLiteDatabase);
+            }
         }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    }
+
+    private void createPredefinedChores(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO " + PREDEFINED_CHORE_TABLE_NAME + " values ('1','Vacuuming','Clean carpet')");
+        db.execSQL("INSERT INTO " + PREDEFINED_CHORE_TABLE_NAME + " values ('2','Dish washing','Wash all utensils')");
+        db.execSQL("INSERT INTO " + PREDEFINED_CHORE_TABLE_NAME + " values ('3','Laundry','Put clothes for washing')");
     }
 }
